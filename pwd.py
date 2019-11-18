842e2a7e7b03622a599810435460ca3b66ef2b77
FrCln <54150197+FrCln@users.noreply.github.com>
2019-11-18 11:45:08 +0300

    1 этап

---
+++ pwd.py
@@ -0,0 +1,23 @@
+while True:
+    print('Введите команду:\n'
+          '1. Ввести новый пароль\n'
+          '2. Получить пароль из базы\n'
+          '3. Выход')
+
+    command = input()
+
+    if command == '1':
+        serv = input('Введите название сервиса: ')
+        pwd = input('Введите пароль: ')
+        with open('pwd.txt', 'a') as f:
+            f.write(serv + ',' + pwd + '\n')
+    elif command == '2':
+        serv = input('Введите название сервиса: ')
+        with open('pwd.txt') as f:
+            for line in f.readlines():
+                s, p = line.split(',')
+                if s == serv:
+                    print(p)
+    elif command == '3':
+        exit()
+    print('\n')
