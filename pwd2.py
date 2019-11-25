0d9fd1f267ccd91152e5523ca4f77860656b1f88
FrCln <54150197+FrCln@users.noreply.github.com>
2019-11-25 11:40:59 +0300

    Exceptions check

--- pwd2.py
+++ pwd2.py
@@ -1,3 +1,25 @@
+base_changed = False
+
+
+def main():
+    funcs = {
+        '1': (add_password, 'Ввести новый пароль'),
+        '2': (get_password, 'Получить пароль из базы'),
+        '3': (get_base, 'Получить список сервисов'),
+        '4': (quit, 'Выход')
+    }
+    passwords = {}
+
+    read_file()
+
+    while True:
+        com = get_command()
+        if com in funcs:
+            funcs[com][0]()
+        else:
+            print('Такой команды нет!')
+
+
 def get_command():
     print('Введите команду:')
     for com in funcs:
@@ -7,10 +29,19 @@ def get_command():
 
 
 def read_file():
-    with open('pwd.txt') as f:
-        for line in f.readlines():
-            s, p = line.strip().split(',')
-            passwords[s] = p
+    try:
+        with open('pwd.txt') as f:
+            for line in f.readlines():
+                s, p = line.strip().split(',')
+                passwords[s] = p
+    except FileNotFoundError:
+        answer = input('Файл базы не найден. Создать новый? (y/n) ')
+        if answer.lower() != 'y':
+            exit()
+    except ValueError:
+        answer = input('Файл базы испорчен. Создать новый? (y/n) ')
+        if answer.lower() != 'y':
+            exit()
 
 
 def save_file():
@@ -20,33 +51,32 @@ def save_file():
 
 
 def add_password():
+    global base_changed
     serv = input('Введите название сервиса: ')
+    if serv in passwords:
+        answer = input(f'Запись {serv} уже есть. Перезаписать? (y/n) ')
+        if answer.lower() != 'y':
+            return
     pwd = input('Введите пароль: ')
-    passwords[serv] = pwd # FIXIT
+    passwords[serv] = pwd
+    base_changed = True
 
 
 def get_password():
     serv = input('Введите название сервиса: ')
-    print(passwords[serv]) # FIXIT
+    try:
+        print(passwords[serv])
+    except KeyError:
+        print(f'Записи {serv} не найдено')
+
+
+def get_base():
+    print('\n'.join(passwords.keys()))
 
 
 def quit():
-    save_file()
+    if base_changed:
+        save_file()
     exit()
 
-
-funcs = {
-    '1': (add_password, 'Ввести новый пароль'),
-    '2': (get_password, 'Получить пароль из базы'),
-    '3': (quit, 'Выход')
-}
-passwords = {}
-
-read_file()
-
-while True:
-    com = get_command()
-    if com in funcs:
-        funcs[com][0]()
-    else:
-        print('Такой команды нет!')
+main()
